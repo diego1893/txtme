3f2ac86cacbbe84bca7fcd8f1a018d031b3cd2cf
Helyer <alexhelyer7@gmail.com>
2018-04-23 04:20:23 -0500

    Se incorporó la funcionalidad de chatear

--- app/src/main/java/com/monsordi/txtme/Message.java
+++ app/src/main/java/com/monsordi/txtme/Message.java
@@ -9,24 +9,47 @@ public class Message {
     private String message;
     private String userId;
     private String date;
+    private String autor;
 
     public Message() {}
 
-    public Message(String message, String userId, String date) {
+    public Message(String message, String userId, String date, String autor) {
         this.message = message;
         this.userId = userId;
         this.date = date;
+        this.autor = autor;
     }
 
     public String getMessage() {
         return message;
     }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     public String getUserId() {
         return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getDate() {
         return date;
     }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
 }
